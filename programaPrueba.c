cd93df8368c4291795a64dfa8e07a5ca65839f64
GabyCodeUAM <87503325+GabyCodeUAM@users.noreply.github.com>
2021-07-27 13:42:11 -0500

    Programa prueba
    Se carga un archivo de prueba

---
+++ programaPrueba.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+  
+  int n1=6,n2=3,res;
+
+  res=n1+n2;
+  printf("La suma es: %d\n",res);
+
+  res=n1-n2;
+  printf("La resta es: %d\n",res);
+
+  res=n1*n2;
+  printf("La multiplicación es: %d\n",res);
+
+  res=n1/n2;
+  printf("La división es: %d\n",res);
+
+  return 0;
+}
